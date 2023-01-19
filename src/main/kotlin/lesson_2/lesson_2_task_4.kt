6152ec68aebceb60ab812682e47a40914bd7bb70
Weirdo28 <skrafi28@gmail.com>
2023-01-19 14:46:03 +0300

    KS-2-4 lesson2 task4 baff

---
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+fun main() {
+    val baff = 0.2F
+    val crystalOre = 7
+    val ironOre = 11
+    val baffKrystal = baff * crystalOre
+    val baffIron = baff * ironOre
+
+    println(baffKrystal.toInt())
+    println(baffIron.toInt())
+}
