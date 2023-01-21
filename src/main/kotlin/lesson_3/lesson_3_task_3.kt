5f1e47de1f779f913fa83f10c652139b8b605f13
Weirdo28 <skrafi28@gmail.com>
2023-01-21 17:31:46 +0300

    KS-3-3 lesson3 task3 multiplication table

--- src/main/kotlin/lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -2,15 +2,16 @@ package lesson_3
 
 fun main() {
 
-    val a = 3
+    val a = 5
 
-    println("$a * 1 = ${a.toInt() * 1}")
-    println("$a * 2 = ${a.toInt() * 2}")
-    println("$a * 3 = ${a.toInt() * 3}")
-    println("$a * 4 = ${a.toInt() * 4}")
-    println("$a * 5 = ${a.toInt() * 5}")
-    println("$a * 6 = ${a.toInt() * 6}")
-    println("$a * 7 = ${a.toInt() * 7}")
-    println("$a * 8 = ${a.toInt() * 8}")
-    println("$a * 9 = ${a.toInt() * 9}")
+    println("""$a * 1 = ${a.toInt() * 1}
+        |$a * 2 = ${a.toInt() * 2}
+        |$a * 3 = ${a.toInt() * 3}
+        |$a * 4 = ${a.toInt() * 4}
+        |$a * 5 = ${a.toInt() * 5}
+        |$a * 6 = ${a.toInt() * 6}
+        |$a * 7 = ${a.toInt() * 7}
+        |$a * 8 = ${a.toInt() * 8}
+        |$a * 9 = ${a.toInt() * 9}
+        |""".trimMargin())
 }
