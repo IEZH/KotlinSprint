748c85ca792de70795cf6f318143a256251821fd
Weirdo28 <skrafi28@gmail.com>
2023-01-21 12:22:09 +0300

    KS-3-3 lesson3 task3 multiplication table

---
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -0,0 +1,16 @@
+package lesson_3
+
+fun main() {
+
+    val a = 3
+
+    println("$a * 1 = ${a.toInt() * 1}")
+    println("$a * 2 = ${a.toInt() * 2}")
+    println("$a * 3 = ${a.toInt() * 3}")
+    println("$a * 4 = ${a.toInt() * 4}")
+    println("$a * 5 = ${a.toInt() * 5}")
+    println("$a * 6 = ${a.toInt() * 6}")
+    println("$a * 7 = ${a.toInt() * 7}")
+    println("$a * 8 = ${a.toInt() * 8}")
+    println("$a * 9 = ${a.toInt() * 9}")
+}
