0d917fca56bf235a48da39664b625e446c25ca0e
Weirdo28 <skrafi28@gmail.com>
2023-01-23 14:17:15 +0300

    KS-3-4 lesson3 task4 chess

---
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+    var whereFrom = "E2"
+    var where = "E4"
+    var stepNumber = 1
+
+    println("$whereFrom $where $stepNumber")
+
+    whereFrom = "D2"
+    where = "D4"
+    stepNumber = 2
+
+    println("$whereFrom $where $stepNumber")
+}
