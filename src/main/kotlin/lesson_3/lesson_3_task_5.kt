7fc44451da70843d0bed854453b6302a56d88414
Weirdo28 <skrafi28@gmail.com>
2023-01-25 21:47:03 +0300

    KS-3-5 lesson3 task5 chess 2

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -3,13 +3,10 @@ package lesson_3
 fun main() {
     val step = "D2-D4;0"
     val whereFrom = step.substringBefore('-')
-    val a = step.substringAfter('-')
-    val where = a.substringBefore(';')
-    val stepNumber = a.substringAfter(';')
+    val where = step.substringAfter('-').substringBefore(';')
+    val stepNumber = step.substringAfter(';')
 
     println(whereFrom)
     println(where)
     println(stepNumber)
-
-
 }
