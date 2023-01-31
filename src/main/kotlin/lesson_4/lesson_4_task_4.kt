fd6ba6ce3763f9d018d869038512a2b2eeaf00f7
Weirdo28 <skrafi28@gmail.com>
2023-01-31 21:37:09 +0300

    KS-4-4 lesson4 task4 fitness

--- src/main/kotlin/lesson_4/lesson_4_task_4.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_4.kt
@@ -2,18 +2,16 @@ package lesson_4
 
 fun main() {
     val day = 5
-    val flag = day % 2
-    val hands = flag == 1
-    val legs = flag != 1
-    val back = flag != 1
-    val press = flag == 1
+    val flag = (day % 2) == 0
+    val hands = !flag
+    val legs = flag
+    val back = flag
+    val press = !flag
 
     println("""
     Упражнения для рук:    $hands
     Упражнения для ног:    $legs
     Упражнения для спины:  $back
     Упражнения для пресса: $press""".trimIndent())
-
-
 
 }
