1bc12ea7ec9a362abeb1e0a2fcbbe3cbe012e4d2
Weirdo28 <skrafi28@gmail.com>
2023-02-05 14:49:53 +0300

    KS-5-4 lesson5 task4 login and password

---
+++ src/main/kotlin/lesson_5/lesson_5_task_4.kt
@@ -0,0 +1,24 @@
+package lesson_5
+
+fun main() {
+// База пользователей:
+    val log1 = "Batman"
+    val pass1 = "password"
+
+    println("Введите свой логин:")
+    val inputLog = readLine()
+
+    if (inputLog != log1)
+        println("Такого логина не существет. Пройдите регистрацию")
+    if (inputLog != log1)
+        return
+
+    println("Введите пароль:")
+    val inputPass = readLine()
+
+    if (inputPass == pass1)
+        println("Добро пожаловать, $log1")
+    else println("Ошибка авторизации. Неверный пароль.")
+
+}
+
