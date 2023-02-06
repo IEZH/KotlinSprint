2f16239daf7095db81474bfb1eed05857912f985
Weirdo28 <skrafi28@gmail.com>
2023-02-06 12:50:59 +0300

    KS-5-3 lesson5 task3 lottery

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -10,9 +10,9 @@ fun main() {
     println("Введите второе число:")
     val numberTwo = readLine()!!.toInt()
 
-    if (numberOne == winningNumberOne && numberTwo == winningNumberTwo)
+    if ((numberOne == winningNumberOne || numberOne == winningNumberTwo) && (numberTwo == winningNumberTwo || numberTwo == winningNumberOne))
         println("Поздравляем! Вы выиграли главный приз!")
-    else if (numberOne == winningNumberOne || numberTwo == winningNumberTwo)
+    else if ((numberOne == winningNumberOne || numberOne == winningNumberTwo) || (numberTwo == winningNumberTwo || numberTwo == winningNumberOne))
         println("Вы выиграли утешительный приз!")
     else println("Неудача! Крутите барабан!")
 }
