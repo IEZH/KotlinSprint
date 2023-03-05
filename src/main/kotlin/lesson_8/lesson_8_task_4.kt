f119a798692946b95f7b7655cff411d15e1d3517
Weirdo28 <skrafi28@gmail.com>
2023-03-05 16:17:07 +0300

    KS-8-4 lesson8 task4 recipes change

---
+++ src/main/kotlin/lesson_8/lesson_8_task_4.kt
@@ -0,0 +1,25 @@
+package lesson_8
+
+fun main() {
+    var recipe = arrayOf("грибы", "курица", "сливки", "масло", "соль", "перец", "лук", "зелень")
+    for (i in recipe) {
+    println("Ингредиент ${recipe.indexOf(i)}: $i")
+    }
+
+    println("Какой ингредиент Вы желаете заменить? (введите порядковый номер)")
+    val oldIngredient = readln().toInt()
+
+    if (oldIngredient !in recipe.indices) {
+        println("Такого ингредиента нет в списке")
+        return
+    }
+
+    println("Укажите, какой ингредиент Вы хотите добавить (введите слово)")
+    val newIngredient = readLine()
+    recipe [oldIngredient] = "$newIngredient"
+
+    println("Готово! Вы сохранили следующий список: ")
+    for (i in recipe) {
+        println("Ингредиент ${recipe.indexOf(i) + 1}: $i")
+    }
+}
