eea7c2fe98bdcf545ffde225407a815fe500b32b
Weirdo28 <skrafi28@gmail.com>
2023-03-08 14:19:16 +0300

    KS-8-3 lesson8 task3 recipes two

--- src/main/kotlin/lesson_8/lesson_8_task_3.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_3.kt
@@ -6,8 +6,7 @@ fun main() {
     println("Введите ингредиент:")
     val ingredient = readLine()
 
-    val availabilityInRecipe = ingredient in recipe
-    if (availabilityInRecipe == true)
+    if (ingredient in recipe)
     println("Ингредиент $ingredient в рецепте есть")
     else println("Такого ингредиента в рецепте нет")
         }
