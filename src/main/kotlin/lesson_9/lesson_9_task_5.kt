a5fe3171195b5b7346cf7855af33bade8c3085a7
Weirdo28 <skrafi28@gmail.com>
2023-03-23 10:49:02 +0300

    KS-9-5 lesson9 task5 five ingredients

---
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -0,0 +1,19 @@
+package lesson_9
+
+fun main() {
+
+    val listOsIngredients: MutableList<String> = mutableListOf()
+
+    for (i in 1..5) {
+        println("Введите ингредиент:")
+        listOsIngredients.add(readln())
+    }
+
+    val listOsIngredientsWithoutDouble = listOsIngredients.toSet().sorted().toMutableList()
+
+    listOsIngredientsWithoutDouble[0].replaceFirstChar { it.titlecase() }
+    listOsIngredientsWithoutDouble[0] = listOsIngredientsWithoutDouble[0].replaceFirstChar { it.titlecase() }
+
+    println(listOsIngredientsWithoutDouble)
+}
+
