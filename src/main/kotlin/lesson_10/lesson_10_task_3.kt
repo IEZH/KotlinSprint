a033c1916bfe7d63ab57a397b8abe65877c8594f
Weirdo28 <skrafi28@gmail.com>
2023-04-02 12:13:53 +0300

    KS-10-3 lesson10 task3 generation password

--- src/main/kotlin/lesson_10/lesson_10_task_3.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_3.kt
@@ -6,13 +6,14 @@ fun main() {
 
     generationPass(passwordLength)
 }
-    fun generationPass(passwordLength: Int): StringBuilder {
-        val pass = StringBuilder()
-        for (i in 1..passwordLength) {
-            val a = ("012345678\"9#\\$%&'()*+,-./:;>=<?@[]^_`~ ").random()
-            pass.append(a)
-        }
 
-        println(pass)
-        return pass
+fun generationPass(passwordLength: Int): StringBuilder {
+    val pass = StringBuilder()
+    for (i in 1..passwordLength) {
+        val a = ("012345678\"9#\\$%&'()*+,-./:;>=<?@[]^_`~ ").random()
+        pass.append(a)
     }
+
+    println(pass)
+    return pass
+}
