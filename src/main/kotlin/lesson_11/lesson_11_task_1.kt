b767fac86592f8ddaff2b9fe47b0a09a0a4e4258
Weirdo28 <skrafi28@gmail.com>
2023-04-14 13:56:04 +0300

    KS-11-1 lesson11 task1 class user data

---
+++ src/main/kotlin/lesson_11/lesson_11_task_1.kt
@@ -0,0 +1,26 @@
+package lesson_11
+
+fun main() {
+    val user1 = User(
+        id = 1,
+        login = "Cat",
+        password = "mymymy",
+        mail = "cat@mail.ru",
+    )
+
+    val user2 = User(
+        id = 2,
+        login = "Dog",
+        password = "afafaf",
+        mail = "dog@mail.ru",
+    )
+    println(user1.id)
+    println(user1.login)
+    println(user1.password)
+    println(user1.mail)
+    println(user2.id)
+    println(user2.login)
+    println(user2.password)
+    println(user2.mail)
+
+}
