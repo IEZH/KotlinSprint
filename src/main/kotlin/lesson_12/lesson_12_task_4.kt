a77e5898a65145c1605676b69a1781365c79b290
Weirdo28 <skrafi28@gmail.com>
2023-07-04 10:23:52 +0300

    KS-12-4 lesson12 task4 weather output

--- src/main/kotlin/lesson_12/lesson_12_task_4.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_4.kt
@@ -1,24 +1,41 @@
 package lesson_12
 
-class WeatherXX(val numberOfDay: Int,
-               val temperatureOfDay: Int,
-               val temperatureOfNight: Int,
-               val atmosphericPressure: Int,
-               val rain: Boolean = false) {
+class WeatherXX(
+    val numberOfDay: Int,
+    val temperatureOfDay: Int,
+    val temperatureOfNight: Int,
+    val atmosphericPressure: Int,
+    val isRain: Boolean = false
+) {
 
     fun displayWeather() {
         println("День $numberOfDay")
         println("Температура днем: $temperatureOfDay")
         println("Температура ночью: $temperatureOfNight")
         println("Атмосферное давление: $atmosphericPressure")
-        println("Дождь: ${if (rain) "да" else "нет"}")
+        println("Дождь: ${if (isRain) "да" else "нет"}")
     }
 }
 
 fun main() {
-    val day1 = WeatherXX(numberOfDay = 1, temperatureOfDay = 30, temperatureOfNight = 26, atmosphericPressure = 751).displayWeather()
+    val day1 = WeatherXX(
+        numberOfDay = 1,
+        temperatureOfDay = 30,
+        temperatureOfNight = 26,
+        atmosphericPressure = 751
+    ).displayWeather()
 
-    val day2 = WeatherXX(numberOfDay = 2, temperatureOfDay = 29, temperatureOfNight = 27, atmosphericPressure = 744).displayWeather()
+    val day2 = WeatherXX(
+        numberOfDay = 2,
+        temperatureOfDay = 29,
+        temperatureOfNight = 27,
+        atmosphericPressure = 744
+    ).displayWeather()
 
-    val day3 = WeatherXX(numberOfDay = 3, temperatureOfDay = 31, temperatureOfNight = 28, atmosphericPressure = 754).displayWeather()
+    val day3 = WeatherXX(
+        numberOfDay = 3,
+        temperatureOfDay = 31,
+        temperatureOfNight = 28,
+        atmosphericPressure = 754
+    ).displayWeather()
 }
