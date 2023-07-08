a3695588bb2e5d6a1f5fe31c86ef67e96b0fe8e2
Weirdo28 <skrafi28@gmail.com>
2023-07-08 15:02:29 +0300

    KS-13-3 lesson13 task3 list of contacts

---
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -0,0 +1,26 @@
+package lesson_13
+
+class ContactY(val name: String, val numberPhone: Long, val company: String? = null)
+
+fun main() {
+    val listOfContacts = mutableListOf<ContactY>()
+
+    val contact1 = ContactY("Ваня бро", 891522511335, null)
+    listOfContacts.add(contact1)
+
+    val contact2 = ContactY("Ден бро", 89265541125, "Сбер")
+    listOfContacts.add(contact2)
+
+    val contact3 = ContactY("Членосос", 89524854877,)
+    listOfContacts.add(contact3)
+
+    printContacts(listOfContacts)
+}
+
+fun printContacts(list: MutableList<ContactY>){
+    list.forEach{
+        println(it.name)
+        println(it.numberPhone)
+        println("${it.company ?: "[не указано]"} \n")
+    }
+}
