178867bbf76aaecf96a1efd290a279a992709d7a
Weirdo28 <skrafi28@gmail.com>
2023-07-09 19:19:10 +0300

    KS-13-2 lesson13 task2 contact with company

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -1,12 +1,12 @@
 package lesson_13
 
-class ContactX(val name: String, val numberPhone: Long, val company: String? = null,) {
+class ContactX(val name: String, val numberPhone: Long, val company: String? = null) {
     fun printInfo() {
         val companyNull = company ?: "[не указано]"
-        println("Имя: $name Номер: $numberPhone Компания: $companyNull")
+        println("Имя: $name \nНомер: $numberPhone \nКомпания: $companyNull")
     }
 }
 
 fun main() {
-    val contact1 = ContactX("Ваня бро",89150114755,null,).printInfo()
+    val contact1 = ContactX("Ваня бро",89150114755,null).printInfo()
 }
